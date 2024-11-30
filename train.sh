7787d449f930b7bdcc5e4f62fd087b70613d8c39
Ethan Siegel <ewsiegel@mit.edu>
2024-11-30 03:40:31 -0500

    lower learning rate

--- train.sh
+++ train.sh
@@ -1,34 +1,35 @@
 python qlora/qlora.py \
     --model_name_or_path huggyllama/llama-13b \
     --output_dir ./output \
-    --do_train True \
-    --do_eval True \
-    --gradient_checkpointing \
-    --dataset data/acad_formatted.csv \
-    --source_max_len 256 \
-    --target_max_len 256 \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 16 \
-    --gradient_accumulation_steps 1 \
-    --logging_steps 10 \
-    --num_train_epochs 5 \
-    --save_strategy epoch \
-    --data_seed 42 \
-    --seed 42 \
-    --save_total_limit 3 \
-    --evaluation_strategy steps \
-    --eval_dataset_size 500 \
-    --max_eval_samples 500 \
-    --eval_steps 200 \
-    --optim paged_adamw_32bit \
     --bits 4 \
     --double_quant \
     --quant_type nf4 \
     --lora_r 8 \
     --lora_alpha 16 \
     --lora_dropout 0.1 \
-    --max_memory_MB 23000 \
     --bf16 True \
     --use_flash_attention_2 True \
-    --preprocess_num_workers 16
+    --dataset data/acad_formatted.csv \
+    --source_max_len 256 \
+    --target_max_len 256 \
+    --preprocess_num_workers 16 \
+    --data_seed 42 \
+    --seed 42 \
+    --max_memory_MB 23000 \
+    --do_train True \
+    --learning_rate 1e-4 \
+    --gradient_checkpointing \
+    --gradient_accumulation_steps 1 \
+    --per_device_train_batch_size 16 \
+    --logging_steps 10 \
+    --num_train_epochs 5 \
+    --optim paged_adamw_32bit \
+    --save_strategy epoch \
+    --save_total_limit 3 \
+    --do_eval True \
+    --per_device_eval_batch_size 16 \
+    --evaluation_strategy steps \
+    --eval_dataset_size 500 \
+    --max_eval_samples 500 \
+    --eval_steps 200 \
 
